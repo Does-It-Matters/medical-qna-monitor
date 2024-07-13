001df0c6e8cf1b86fe95dd552ac27d5a1c30f6b7
glass <silver20190204@gmail.com>
2024-07-14 01:42:14 +0900

    Add webflux controller (Issue #1)

---
+++ src/main/java/com/example/monitoring/MedicalQnaMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalQnaMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalQnaMonitorApplication.class, args);
+	}
+
+}
