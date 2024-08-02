a4d70b4b3e6b6c16ea80bb3d01a2b5391c2b2f6a
glass <silver20190204@gmail.com>
2024-08-02 21:42:09 +0900

    Remove webflux code and dependencies

--- src/main/java/com/example/monitoring/webflux/exercise/monitor/cpu/CpuMonitoringController.java
+++ src/main/java/com/example/monitoring/webflux/exercise/monitor/cpu/CpuMonitoringController.java
@@ -1,27 +1,27 @@
-package com.example.monitoring.webflux.exercise.monitor.cpu;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/api/cpu")
-public class CpuMonitoringController {
-    @Autowired
-    private CpuMonitoringService service;
-
-    @GetMapping("/v1/load")
-    public Mono<Map<String, Double>> getSystemLoad() {
-        return service.getSystemLoadAvg()
-                .map(avg -> {
-                    Map<String, Double> load = new HashMap<>();
-                    load.put("loadAvg", avg);
-                    return load;
-                });
-    }
-}
+//package com.example.monitoring.webflux.exercise.monitor.cpu;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/api/cpu")
+//public class CpuMonitoringController {
+//    @Autowired
+//    private CpuMonitoringService service;
+//
+//    @GetMapping("/v1/load")
+//    public Mono<Map<String, Double>> getSystemLoad() {
+//        return service.getSystemLoadAvg()
+//                .map(avg -> {
+//                    Map<String, Double> load = new HashMap<>();
+//                    load.put("loadAvg", avg);
+//                    return load;
+//                });
+//    }
+//}
--- src/main/java/com/example/monitoring/webflux/exercise/monitor/cpu/CpuMonitoringService.java
+++ src/main/java/com/example/monitoring/webflux/exercise/monitor/cpu/CpuMonitoringService.java
@@ -1,21 +1,21 @@
-package com.example.monitoring.webflux.exercise.monitor.cpu;
-
-import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
-
-@Service
-public class CpuMonitoringService {
-
-    private final OperatingSystemMXBean osBean;
-
-    public CpuMonitoringService() {
-        this.osBean = ManagementFactory.getOperatingSystemMXBean();
-    }
-
-    public Mono<Double> getSystemLoadAvg() {
-        return Mono.fromCallable(osBean::getSystemLoadAverage);
-    }
-}
+//package com.example.monitoring.webflux.exercise.monitor.cpu;
+//
+//import org.springframework.stereotype.Service;
+//import reactor.core.publisher.Mono;
+//
+//import java.lang.management.ManagementFactory;
+//import java.lang.management.OperatingSystemMXBean;
+//
+//@Service
+//public class CpuMonitoringService {
+//
+//    private final OperatingSystemMXBean osBean;
+//
+//    public CpuMonitoringService() {
+//        this.osBean = ManagementFactory.getOperatingSystemMXBean();
+//    }
+//
+//    public Mono<Double> getSystemLoadAvg() {
+//        return Mono.fromCallable(osBean::getSystemLoadAverage);
+//    }
+//}
